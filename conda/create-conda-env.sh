659b5a7a2919dd99e3dd47c329618e51a73c728b
Oriol Fornes <oriol.fornes@gmail.com>
2020-09-24 11:23:48 -0700

    updated conda environment

--- conda/create-conda-env.sh
+++ conda/create-conda-env.sh
@@ -4,8 +4,8 @@
 eval "$(conda shell.bash hook)"
 
 # Create conda environment
-conda create -n JASPAR-MoDisco -c bioconda -c conda-forge biopython logomaker \
-    python=3.6.11 rsat-core=2020.02.29 snakemake=4.0.0
+conda create -n JASPAR-MoDisco -c bioconda -c conda-forge biopython=1.78 \
+    logomaker=0.8 python=3.6.11 rsat-core=2020.02.29 snakemake=4.0.0
 
 # Install PWMScan binaries
 # https://ccg.epfl.ch/pwmtools/pwmscan.php

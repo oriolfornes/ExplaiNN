2a3bcd59ae3d38b6ac0b0ec6f0566df3fd4695e0
Oriol Fornes <oriol.fornes@gmail.com>
2020-09-28 11:09:11 -0700

    updated pcm2pwm

--- workflow/scripts/pcm2pwm.py
+++ workflow/scripts/pcm2pwm.py
@@ -18,6 +18,12 @@ def parse_args():
 
     # Mandatory args
     parser.add_argument("motif_file", metavar="motif.jaspar")
+    parser.add_argument(
+        "--format",
+        choices=["pwm", "meme"],
+        default="pwm",
+        help="output format (default: pwm)"
+    )
 
     return(parser.parse_args())
 
@@ -31,8 +37,30 @@ def main():
     m.pseudocounts = motifs.jaspar.calculate_pseudocounts(m)
 
     # Print PWM
-    for row in np.transpose(np.array(list(m.pssm.values()))):
-        print(" ".join([str(round(r, 8)).rjust(11) for r in row]))
+    if args.format == "meme":
+        consensus = m.pssm.consensus
+        w = len(consensus)
+        nsites = int(sum([m.counts[n][0] for n in "ACGT"]))
+        print("MEME version 4")
+        print()
+        print("ALPHABET= ACGT")
+        print()
+        print("strands: + -")
+        print()
+        print("Background letter frequencies (from uniform background):")
+        print("A 0.25000 C 0.25000 G 0.25000 T 0.25000")
+        print()
+        print("MOTIF 1 %s" % consensus)
+        print()
+        print(
+            "letter-probability matrix: alength= 4 w= %s nsites= %s E= 0" \
+            % (w, nsites)
+        )
+        for row in np.transpose(np.array(list(m.pwm.values()))):
+            print(" ".join([str(round(r, 8)).rjust(11) for r in row]))
+    else:
+        for row in np.transpose(np.array(list(m.pssm.values()))):
+            print(" ".join([str(round(r, 8)).rjust(11) for r in row]))
 
 #-------------#
 # Main        #

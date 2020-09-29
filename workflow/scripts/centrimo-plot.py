88f73a8146e8673c581c562815ecf4676a3f39c4
Oriol Fornes <oriol.fornes@gmail.com>
2020-09-29 18:33:48 +0000

    updated centrimo plot

--- workflow/scripts/centrimo-plot.py
+++ workflow/scripts/centrimo-plot.py
@@ -36,7 +36,35 @@ def main():
     # Parse arguments
     args = parse_args()
 
-    # Get centrimo plot
+    # Read p-value
+    with open(args.centrimo_file, "r") as f:
+        for line in f:
+            if line.startswith("   1"):
+                line = line.split("\t")
+                p = Decimal(line[5])
+                break
+
+    # Read counts
+    df = pd.read_csv(
+        args.counts_file,
+        # names=["Distance to peak centre", "Number of motif occurrences"],
+        names=["x", "y"],
+        sep="\t",
+        skiprows=1
+    )
+
+    # Plot centrality
+    fig, ax = plt.subplots()
+    ax.plot(list(df["x"]), list(df["y"]/df["y"].max()))
+    ax.set_xlabel("Distance to peak centre", fontproperties=prop)
+    ax.set_xlim(-500, 500)
+    ax.xaxis.set_ticks([-500, -250, 0, 250, 500])
+    ax.set_xticklabels(labels=[-500, -250, 0, 250, 500], fontproperties=prop)
+    ax.set_ylabel("Relative number of motif occurrences", fontproperties=prop)
+    ax.set_ylim(0, 1)
+    ax.yaxis.set_ticks([0.0, 0.25, 0.5, 0.75, 1.0])
+    ax.set_xticklabels(labels=[0.0, 0.25, 0.5, 0.75, 1.0], fontproperties=prop)
+    ax.text(-450, .9, str(p))
 
     # Save logo
     fig.savefig(args.pdf_file)

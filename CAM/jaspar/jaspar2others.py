2ec9001c174908d18df5ac6df89bf0f8374a87c7
Oriol Fornes <oriol.fornes@gmail.com>
2021-07-12 13:30:04 -0700

    updates updates

--- CAM/jaspar/jaspar2others.py
+++ CAM/jaspar/jaspar2others.py
@@ -35,35 +35,44 @@ def parse_args():
 def reformat_jaspar_motif(motif_file, oformat, out_file):
 
     # Initialize
-    s = ""
+    l = []
 
     # From https://biopython.readthedocs.io/en/latest/chapter_motifs.html
-    m = motifs.read(open(motif_file), "jaspar")
-    if oformat != "pcm":
-        m.pseudocounts = motifs.jaspar.calculate_pseudocounts(m)
+    for m in motifs.parse(open(motif_file), "jaspar"):
+        l.append(m)
+
+    reformat_motifs(l, oformat, out_file)
+
+def reformat_motifs(l, oformat, out_file):
+
+    # Initialize
+    s = ""
 
     # Print
     if oformat == "meme":
-        name = m.name
-        consensus = m.pssm.consensus
-        w = len(consensus)
-        nsites = int(sum([m.counts[n][0] for n in "ACGT"]))
         s += "MEME version 4\n\n"
         s += "ALPHABET= ACGT\n\n"
         s += "strands: + -\n\n"
         s += "Background letter frequencies (from uniform background):\n"
-        s += "A 0.25000 C 0.25000 G 0.25000 T 0.25000\n\n"
-        s += "MOTIF %s %s\n" % (name, consensus)
-        s += "letter-probability matrix: alength= 4 w= %s nsites= %s E= 0" % \
-            (w, nsites)
-        for row in np.transpose(np.array(list(m.pwm.values()))):
-            s+= " ".join([str(round(r, 8)).rjust(11) for r in row]) + "\n"
-    elif oformat == "pcm":
-        for row in np.transpose(np.array(list(m.counts.values()))):
-            s+= "\t".join(list(map(str, list(map(int, row))))) + "\n"
-    else:
-        for row in np.transpose(np.array(list(m.pssm.values()))):
-            s+= " ".join([str(round(r, 8)).rjust(11) for r in row]) + "\n"
+        s += "A 0.25000 C 0.25000 G 0.25000 T 0.25000\n"
+    for m in l:
+        if oformat == "meme":
+            name = m.name
+            consensus = m.pssm.consensus
+            w = len(consensus)
+            nsites = int(sum([m.counts[n][0] for n in "ACGT"]))
+            s += "\nMOTIF %s %s\n" % (name, consensus)
+            s += "letter-probability matrix: alength= 4 w= %s nsites= %s E= 0\n" % \
+                (w, nsites)
+            for row in np.transpose(np.array(list(m.pwm.values()))):
+                s+= " ".join([str(round(r, 8)).rjust(11) for r in row]) + "\n"
+        elif oformat == "pcm":
+            m.pseudocounts = motifs.jaspar.calculate_pseudocounts(m)
+            for row in np.transpose(np.array(list(m.counts.values()))):
+                s+= "\t".join(list(map(str, list(map(int, row))))) + "\n"
+        else:
+            for row in np.transpose(np.array(list(m.pssm.values()))):
+                s+= " ".join([str(round(r, 8)).rjust(11) for r in row]) + "\n"
 
     # Write
     if out_file is None:

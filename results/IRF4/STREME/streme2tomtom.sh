ff3794c1a7b1435bc38693b66eea6e7c16e8f7f7
oriolfornes <oriol.fornes@gmail.com>
2021-10-25 14:49:10 -0700

    added HT-SELEX results T95R

--- results/IRF4/STREME/streme2tomtom.sh
+++ results/IRF4/STREME/streme2tomtom.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-tomtom -oc WT.tomtom WT/streme.txt JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt
-tomtom -oc T95R.tomtom T95R/streme.txt JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt
-tomtom -oc WT.201bp.tomtom WT.201bp/streme.txt JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt
-tomtom -oc T95R.201bp.tomtom T95R.201bp/streme.txt JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt
-tomtom -oc WT-unique.tomtom WT-unique/streme.txt JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt
-tomtom -oc T95R-unique.tomtom T95R-unique/streme.txt JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt
-tomtom -oc T95R-WT-intersect.tomtom T95R-WT-intersect/streme.txt JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt
+tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc WT.tomtom WT/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme
+tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc T95R.tomtom T95R/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme
+tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc WT.201bp.tomtom WT.201bp/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme
+tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc T95R.201bp.tomtom T95R.201bp/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme
+tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc WT-unique.tomtom WT-unique/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme
+tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc T95R-unique.tomtom T95R-unique/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme
+tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc T95R-WT-intersect.tomtom T95R-WT-intersect/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme

d0d124363ef8f630a9d4711bdf2b3d9ea336495b
oriolfornes <oriol.fornes@gmail.com>
2021-10-26 10:26:48 -0700

    added affi-seq and ht-selex IRF4

--- results/IRF4/STREME/fasta2streme.sh
+++ results/IRF4/STREME/fasta2streme.sh
@@ -2,8 +2,8 @@
 
 FASTA_DIR=${HOME}/CAM/resources/IRF4/ChIP-seq
 
-streme --p ${FASTA_DIR}/WT.fa --oc WT --maxw 21 --nmotifs 64
-streme --p ${FASTA_DIR}/T95R.fa --oc T95R --maxw 21 --nmotifs 64
-streme --p ${FASTA_DIR}/WT-unique.fa --oc WT-unique --maxw 21 --nmotifs 64
-streme --p ${FASTA_DIR}/T95R-unique.fa --oc T95R-unique --maxw 21 --nmotifs 64
-streme --p ${FASTA_DIR}/T95R-WT-intersect.fa --oc T95R-WT-intersect --maxw 21 --nmotifs 64
+streme --p ${FASTA_DIR}/WT.fa --oc WT --maxw 21
+streme --p ${FASTA_DIR}/T95R.fa --oc T95R --maxw 21
+streme --p ${FASTA_DIR}/WT-unique.fa --oc WT-unique --maxw 21
+streme --p ${FASTA_DIR}/T95R-unique.fa --oc T95R-unique --maxw 21
+streme --p ${FASTA_DIR}/T95R-WT-intersect.fa --oc T95R-WT-intersect --maxw 21

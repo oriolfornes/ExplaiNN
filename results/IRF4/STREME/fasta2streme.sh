de13b87c6ac2d7291b6e25075f65a5fc0ef79983
oriolfornes <oriol.fornes@gmail.com>
2021-10-25 19:11:04 -0700

    added ChIP-seq WT

--- results/IRF4/STREME/fasta2streme.sh
+++ results/IRF4/STREME/fasta2streme.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#streme --p WT.fa --oc WT --maxw 21
-#streme --p T95R.fa --oc T95R --maxw 21 
-#streme --p WT.201bp.fa --oc WT.201bp --maxw 21
-#streme --p T95R.201bp.fa --oc T95R.201bp --maxw 21
-streme --p WT-unique.fa --oc WT-unique --maxw 21
-streme --p T95R-unique.fa --oc T95R-unique --maxw 21
-streme --p T95R-WT-intersect.fa --oc T95R-WT-intersect --maxw 21
+FASTA_DIR=${HOME}/CAM/resources/IRF4/ChIP-seq
+
+streme --p ${FASTA_DIR}/WT.fa --oc WT --maxw 21 --nmotifs 64
+streme --p ${FASTA_DIR}/T95R.fa --oc T95R --maxw 21 --nmotifs 64
+streme --p ${FASTA_DIR}/WT-unique.fa --oc WT-unique --maxw 21 --nmotifs 64
+streme --p ${FASTA_DIR}/T95R-unique.fa --oc T95R-unique --maxw 21 --nmotifs 64
+streme --p ${FASTA_DIR}/T95R-WT-intersect.fa --oc T95R-WT-intersect --maxw 21 --nmotifs 64
--- results/IRF4/STREME/streme2tomtom.sh
+++ results/IRF4/STREME/streme2tomtom.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+FASTA_DIR=
+
 tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc WT.tomtom WT/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme
 tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc T95R.tomtom T95R/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme
 tomtom -no-ssc -verbosity 1 -min-overlap 5 -dist pearson -evalue -thresh 10.0 -oc WT.201bp.tomtom WT.201bp/streme.txt db/JASPAR/JASPAR2018_CORE_vertebrates_non-redundant.meme db/MOUSE/uniprobe_mouse.meme db/EUKARYOTE/jolma2013.meme
